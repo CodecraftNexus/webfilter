ea0e331efa16af292cd19943fedaae5c7e2dbe71
CodecraftNexus <105867288+CodecraftNexus@users.noreply.github.com>
2026-02-23 06:51:00 +0530

    main

--- dist/db/relationships/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.initRelationship = initRelationship;
-const User_1 = require("./User");
-function initRelationship(models) {
-    (0, User_1.initUserRelationship)(models);
-}
--- dist/validators/updateProfile.validator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateProfileSchema = void 0;
-const zod_1 = require("zod");
-exports.updateProfileSchema = zod_1.z.object({
-    gender: zod_1.z.enum(["Male", "Female", "Other", ""]).optional(),
-    profileImage: zod_1.z.string().optional(),
-    language: zod_1.z.string().optional(),
-});
